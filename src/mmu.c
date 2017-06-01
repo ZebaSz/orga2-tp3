21b9180f7351e8973822b35f6a6301aaed49a34d
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-06-01 20:22:54 -0300

    Mover structs de mmu a .h

--- src/mmu.c
+++ src/mmu.c
@@ -6,36 +6,6 @@
 */
 
 #include "mmu.h"
-#include "defines.h"
-
-typedef struct pte_s {
-    unsigned char   p:1;
-    unsigned char   rw:1;
-    unsigned char   us:1;
-    unsigned char   pwt:1;
-    unsigned char   pcd:1;
-    unsigned char   a:1;
-    unsigned char   d:1;
-    unsigned char   pat:1;
-    unsigned char   g:1;
-    unsigned char   avl:3;
-    unsigned int    page_addr:20;
-} pt_entry;
-
-typedef struct pde_s {
-    unsigned char   p:1;
-    unsigned char   rw:1;
-    unsigned char   us:1;
-    unsigned char   pwt:1;
-    unsigned char   pcd:1;
-    unsigned char   a:1;
-    unsigned char   ign:1;
-    unsigned char   ps:1;
-    unsigned char   g:1;
-    unsigned char   avl:3;
-    unsigned int    page_addr:20;
-} pd_entry;
-
 
 void mmu_inicializar() {
 	
