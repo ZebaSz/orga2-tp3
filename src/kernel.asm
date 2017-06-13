0ae26b79d41f406d54602b3bb14203ffd1439bad
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-06-13 18:52:38 -0300

    Agregar mappeo (NO TESTEADO) de memoria de zombies

--- src/kernel.asm
+++ src/kernel.asm
@@ -80,16 +80,13 @@ BITS 32
     ; Inicializar pantalla
     call print_mapa
     ; Inicializar el manejador de memoria
- 
+    call mmu_inicializar
     ; Inicializar el directorio de paginas
     call mmu_inicializar_dir_kernel
     ; Cargar directorio de paginas
-
-
-    ; Habilitar paginacion
-    mov eax, 0x27000 ;PAGE_DIRECTORY
+    mov eax, 0x27000
     mov cr3, eax
-    
+    ; Habilitar paginacion
     mov eax, cr0
     or eax, 0x80000000
     mov cr0, eax
@@ -103,8 +100,6 @@ BITS 32
     call idt_inicializar
     ; Cargar IDT
     lidt [IDT_DESC]
-    ; ERROR DE PRUEBA
-    ;mov dword [fs:0xFFFFFF], 0xFA50
  
     ; Configurar controlador de interrupciones
 
